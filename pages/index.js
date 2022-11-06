bd7eb8d333c00fd18688e2cf75bdd362c3e3298d
Ryohei Matsuba <drumacho@gmail.com>
2022-11-06 16:47:54 +0900

    Layoutコンポーネント作成

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Header from "components/header";
+import Layout from "components/layout";
 import Hero from "components/hero";
-import Footer from "components/footer";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-
-      <main>
-        <Hero />
-      </main>
-
-      <Footer />
-    </>
+    <Layout>
+      <Hero />
+    </Layout>
   )
 }

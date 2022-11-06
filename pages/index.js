91d25c3a1204d1a2667b5f59d5f5fdc92aa9d0da
Ryohei Matsuba <drumacho@gmail.com>
2022-11-06 16:39:34 +0900

    絶対パスでインポートできるようにする

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Header from "../components/header";
-import Hero from "../components/hero";
-import Footer from "../components/footer";
+import Header from "components/header";
+import Hero from "components/hero";
+import Footer from "components/footer";
 
 export default function Home() {
   return (

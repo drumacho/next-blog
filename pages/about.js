84f8016539488aaeac2f16ea51f597b545e27132
Ryohei Matsuba <drumacho@gmail.com>
2023-04-29 21:38:48 +0900

    contactコンポーネント作成

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import Container from "components/container";
 import Hero from "components/hero";
 import PostBody from "components/post-body";
+import Contact from "components/contact";
 
 export default function About() {
   return (
@@ -29,6 +30,8 @@ export default function About() {
           Cubeが得意とする分野はモノづくりです。3次元から2次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
         </p>
       </PostBody>
+
+      <Contact />
     </Container>
   )
 }
